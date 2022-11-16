d2258253d7ebadde2f5d2ddd0b83ce9e28f9fabd
shrutivtu <shrutisaxena1596@gmail.com>
2022-11-16 23:44:33 +0530

    Feat: Header basic layout

---
+++ components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../styles/Header.module.scss';
+
+const Header = () => {
+  return (
+    <section className={styles.headerContainer}>
+        <div>Felix</div>
+    </section>
+  )
+}
+
+export default Header;
