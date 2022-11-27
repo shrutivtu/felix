8b86d2d6ba44f498e6fe644df03f2b5af03477ac
shrutivtu <shrutisaxena1596@gmail.com>
2022-11-27 20:04:56 +0530

    Feat: My Day Left Section enhancement in styling

--- components/MyDay/MyDayLeft.js
+++ components/MyDay/MyDayLeft.js
@@ -11,7 +11,7 @@ const MyDayLeft = ({ styles }) => {
         },
         {
             id: uuidv4,
-            taskDesc: 'Finish this page',
+            taskDesc: 'Finish this page.',
             editable: true,
             check: false
         }
@@ -22,13 +22,13 @@ const MyDayLeft = ({ styles }) => {
                 Tasks that I have to do today
             </h3>
             <div className={styles.inputBtnSection}>
-                <input type="text" className={styles.inputLeft} />
+                <input type="text" className={styles.inputLeft} placeholder="Add Task...." />
                 <div className={styles.buttonLeft}><span>Add</span></div>
             </div>
             <div className={styles.taskList}>
                {tasks.map(task => {
-                    return (<div key={task.id} className={styles.taskSection}>
-                        <span>{task.taskDesc}</span>
+                    return (<div key={task.id} className={styles.taskBox}>
+                        <input type="checkbox" /><span className={styles.taskDesc}>{task.taskDesc}</span>
                     </div>)
                 })}
             </div>
