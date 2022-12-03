2b0c16a69c3c1a2a5d412d825b7811f82c1b4e25
shrutivtu <shrutisaxena1596@gmail.com>
2022-12-03 22:46:53 +0530

    Feat: medayleft section addition of task issue fix and check handler implemented

--- components/MyDay/MyDayLeft.js
+++ components/MyDay/MyDayLeft.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const MyDayLeft = ({ styles }) => {
-    const tasks = [
-        {
-            id: uuidv4,
-            taskDesc: 'Pick some flowers',
-            editable: true,
-            check: false
-        },
-        {
-            id: uuidv4,
-            taskDesc: 'Finish this page.',
-            editable: true,
-            check: false
-        }
-    ]
+    const [haveToDoTasks, setHaveToDoTasks] = useState([]);
+    const [taskDesc, setTaskDesc] = useState('');
 
-    const handleCheck = () => {
-        console.log('checked');
+    const handleCheck = (id) => {
+        // let actionTask = haveToDoTasks.filter((task) => task.id == id);
+        // let status = actionTask[0].check;
+        // actionTask[0].check = !status;
+
+        let updatedTasks = haveToDoTasks.map(task => {
+            if(task.id !== id){
+                return task;
+            }
+            else{
+                let status = task.check;
+                task.check = !status;
+                return task;
+            }
+        })
+        setHaveToDoTasks(updatedTasks);
     }
 
     const handleAdd = () => {
-        console.log('add task');
+        const obj = {
+            id: uuidv4(),
+            taskDesc: taskDesc,
+            editable: true,
+            check: false
+        }
+        setHaveToDoTasks([...haveToDoTasks, obj]);
     }
     return (
         <div>
@@ -30,16 +38,23 @@ const MyDayLeft = ({ styles }) => {
                 Tasks that I have to do today
             </h3>
             <div className={styles.inputBtnSection}>
-                <input type="text" className={styles.inputLeft} placeholder="Add Task...." />
+                <input type="text" className={styles.inputLeft} placeholder="Add Task...." onChange={(e) => setTaskDesc(e.target.value)}/>
                 <div className={styles.buttonLeft} onClick={handleAdd}><span>Add</span></div>
             </div>
-            <div className={styles.taskList}>
-               {tasks.map(task => {
-                    return (<div key={task.id} className={styles.taskBox}>
-                        <input type="checkbox" onChange={handleCheck}/><span className={styles.taskDesc}>{task.taskDesc}</span>
-                    </div>)
+            {haveToDoTasks.length > 0 && <div className={styles.taskList}>
+               {haveToDoTasks.map(task => {
+                    if(!task.check){
+                        return (<div key={task.id} className={styles.taskBox}>
+                            <input type="checkbox" onChange={() => handleCheck(task.id)} id={task.id} /><span className={styles.taskDesc}>{task.taskDesc}</span>
+                        </div>)
+                    }
+                    else{
+                        return (<div key={task.id} className={`${styles.taskBox} ${styles.checkTaskBox}`}>
+                            <input type="checkbox" onChange={() => handleCheck(task.id)} id={task.id} /><span className={styles.taskDesc}>{task.taskDesc}</span>
+                        </div>)
+                    }
                 })}
-            </div>
+            </div>}
         </div>
     )
 }
