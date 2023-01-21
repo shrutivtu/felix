ca578091dbfc814168f6008898aaac83c730a74f
shrutivtu <shrutisaxena1596@gmail.com>
2023-01-21 17:17:15 +0530

    Feat: store setup

--- context/index.js
+++ context/index.js
@@ -1,17 +1,21 @@
 import { createContext, useState, useEffect } from "react";
-
 const FelixContext = createContext(undefined);
-
 const FelixContextProvider = (props) => {
-
   const [ targets, setTargets ] = useState([]);
-
+  useEffect(() => {
+    console.log(targets);
+  },[targets]);
+  const addTarget = (target) => {
+    let t = targets;
+    t.push(target);
+    console.log(t);
+    setTargets(t);
+  }
   return(
-    <FelixContext.Provider value={{targets, setTargets}}>
+    <FelixContext.Provider value={{targets, setTargets, addTarget}}>
       {props.children}
     </FelixContext.Provider>
   )
 }
-
 export { FelixContext };
 export default FelixContextProvider;
